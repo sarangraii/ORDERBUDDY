7d5ac37e3c4f5df35a36966bd16f1cbbd6c324c7
sarangraii <sarang2452@gmail.com>
2026-02-27 16:10:30 +0530

    fix: use preview server for production

--- apps/order/vite.config.ts
+++ apps/order/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 10000,
+    host: true
+  },
   server: {
     port: 5173,
     proxy: {
